f74480ef18e7675f8ceba5546bcf53f3a2f2db2a
PedroGomes2k <ph.gomes2k@gmail.com.br>
2023-10-16 17:19:36 -0300

    feat: Update the project

--- src/repository/posts.repository.js
+++ src/repository/posts.repository.js
@@ -5,13 +5,13 @@ export async function allPost() {
     return (
         db.query(`
             SELECT
-                users.name AS "username",
+                users.username AS "username",
                 users.photo,
                 posts.link,
                 posts.description,
                 posts.likes
             FROM posts
-            JOIN users ON users.id = posts."userId";`)
+            JOIN users ON users.id = posts."userid";`)
     )
 }
 
@@ -27,7 +27,7 @@ export async function createPost(userId, url, description) {
 
     return (
         db.query(`
-        INSERT INTO posts ("userId", link, description)
+        INSERT INTO posts ("userid", link, description)
         VALUES ($1,$2,$3);
         `, [userId, url, description])
     )
@@ -38,14 +38,14 @@ export async function userPosts(userId) {
     return (
         db.query(`
             SELECT
-                users.name AS "username",
+                users.username AS "username",
                 users.photo,
                 posts.link,
                 posts.description,
                 posts.likes
             FROM posts
-            JOIN users ON users.id = posts."userId"
-            WHERE "userId" = $1;
+            JOIN users ON users.id = posts."userid"
+            WHERE "userid = $1;
         ;`, [userId])
     )
 }
